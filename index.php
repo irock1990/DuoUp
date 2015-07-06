684194229cf4532e993c715efbad4986e00bc64f
irock1990 <aaairock@gmail.com>
2015-07-05 22:18:07 -0400

    login test
    upload login tsest

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
-<?php 
 
-echo 'Hello World';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+</head>
+<body>
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '671523772980607',
+	      xfbml      : true,
+	      version    : 'v2.3'
+	    });
+	  };
 
-echo 'This is a GitHub Test';
-
-?>
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>	
+</body>
+</html>
