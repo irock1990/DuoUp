6626c528cf612846785cd6af77423cb331635b15
irock1990 <aaairock@gmail.com>
2015-07-06 01:07:00 -0400

    setting you up
    you're welcome

--- index.php
+++ index.php
@@ -23,6 +23,28 @@
 	     js.src = "//connect.facebook.net/en_US/sdk.js";
 	     fjs.parentNode.insertBefore(js, fjs);
 	   }(document, 'script', 'facebook-jssdk'));
+
+	  /* 4-16 character summoner name use Riot API to check after checking DB first */
 	</script>	
+	<div>
+		<input type="text" id="name" />
+		<select>
+		  <option value="bronze">Bronze</option>
+		  <option value="silver">Silver</option>
+		  <option value="gold">Gold</option>
+		  <option value="platinum">Platinum</option>
+		  <option value="diamond">Diamond</option>
+		</select>
+		<select>
+		  <option value="i">I</option>
+		  <option value="ii">II</option>
+		  <option value="iii">III</option>
+		  <option value="iv">IV</option>
+		  <option value="v">V</option>
+		</select>
+		<button id="search_button">Search</button>
+	</div>
+	
+
 </body>
 </html>
