191f5df4f0458d475bb7ee1d54792d7f3bb53220
irock1990 <aaairock@gmail.com>
2015-07-08 22:22:52 -0400

    boob
    boob

--- find_partner.php
+++ find_partner.php
@@ -1,15 +1,15 @@
 <?php
 
-
-
-if(  isset($_GET["id"]) && isset($_GET["n"]) && isset($_GET["re"]) && isset($_GET["ra"]) && isset($_GET["l"]) && isset($_GET["p"]) ) {
-
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 	$server = $url["host"];
 	$username = $url["user"];
 	$password = $url["pass"];
 	$db = substr($url["path"], 1);
+
+if(  isset($_GET["id"]) && isset($_GET["n"]) && isset($_GET["re"]) && isset($_GET["ra"]) && isset($_GET["l"]) && isset($_GET["p"]) ) {
+
+
 
 	$conn = new mysqli($server, $username, $password, $db);
 
